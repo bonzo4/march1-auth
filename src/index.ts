38ffed3da281db2d60a6cc4abdb466d1e7a3545d
bonzo4 <alonzo.sabino814@gmail.com>
2025-02-17 13:12:56 -0600

    feat: package elysia type

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,10 @@ import { sendOTPRoute } from "./route/sendOTP";
 import { verifyOTPRoute } from "./route/verifyOTP";
 import { healthCheckRoute } from "./route/healthCheck";
 
-const app = new Elysia()
+const authApi = new Elysia()
   .use(healthCheckRoute)
   .use(sendOTPRoute)
   .use(verifyOTPRoute)
   .listen(process.env.PORT!);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+export type AuthApiType = typeof authApi;
